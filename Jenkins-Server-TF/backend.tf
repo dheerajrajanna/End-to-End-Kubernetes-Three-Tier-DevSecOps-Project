00a73a2c7a6b7d66323c6d4aabc09484baa50553
Dheeraj <63892608+dheerajrajanna@users.noreply.github.com>
2026-03-03 20:59:10 +0530

    Update S3 bucket and DynamoDB table names

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-dakshbhuvi"
+    bucket         = "dakshbhuvi"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "db-Lock-files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
