58d8a3781990b7178fdca2b9daef7544173a0ab8
Dheeraj <63892608+dheerajrajanna@users.noreply.github.com>
2026-03-04 18:11:00 +0530

    Update Jenkins installation commands in script

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -9,11 +9,15 @@ java --version
 # Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian binary/" | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
 sudo apt-get install jenkins -y
+
+# Enable and start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
 # Installing Docker 
 #!/bin/bash
