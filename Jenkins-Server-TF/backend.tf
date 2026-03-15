fc2c31bcc86d200bd6907a2cfcafe1e90fa68abb
Dheeraj <63892608+dheerajrajanna@users.noreply.github.com>
2026-03-15 12:07:46 +0530

    Remove dynamodb_table from backend configuration
    Removed dynamodb_table configuration from backend.

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "dakshbhuvi1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
   required_version = ">=0.13.0"
